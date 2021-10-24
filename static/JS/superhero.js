21d2e495d28407ed3f1f110f0d624bd9750b61b7
John Matelyan <jm628314@gmail.com>
2021-10-24 13:32:52 -0400

    Superhero Banner

--- static/JS/superhero.js
+++ static/JS/superhero.js
@@ -1,18 +1,37 @@
-window.onload = rotate;
 
-let theAd = 0; 
-
-let imgs = new Array('imgs/superhero1.png', 'imgs/superhero2.png', 'imgs/superhero3.png', 'imgs/superhero4.png', 
-'imgs/superhero5.png', 'imgs/superhero6.png');
-
-function rotate(){
-    theAd ++;
-    if (theAd == imgs.length){
-        theAd = 0;
+"use strict";
+const $ = selector => document.querySelector(selector);
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const caption = $("#caption");        // the h2 element for the caption
+    const mainImage = $("#main_image");   // the img element for the show
+        
+    // get all the <a> tags in the ul element
+    const links = $("#image_list").querySelectorAll("a");
+    
+    // Process image links
+    const imageCache = [];
+    let image = null;
+    for ( let link of links ) {
+        // Preload image and copy title properties
+        image = new Image();
+        image.src = link.href;
+        image.alt = link.title;
+        // add image to array 
+        imageCache[imageCache.length] = image;
     }
-    document.getElementById("addBanner").src = imgs[theAd];
-}
-
-setTimeout(rotate, 3* 1000);
-
-
+    // Start slide show
+    let imageCounter = 0;
+    setInterval( () => { 
+        // calculate the index for the current image
+        imageCounter = (imageCounter + 1) % imageCache.length;
+        console.log("imageCounter=" + imageCounter); 
+        // get image from array
+        image = imageCache[imageCounter];
+        // set HTML img and h2 with values from image object
+        mainImage.src = image.src;
+        mainImage.alt = image.alt;
+        caption.textContent = image.alt;
+    },
+    2000);  // 2 second interval
+});
